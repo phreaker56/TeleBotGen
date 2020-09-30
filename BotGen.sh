95a8ddd757b74cb3bcfe34f02f475aca24366a95
rudi9999 <67137156+rudi9999@users.noreply.github.com>
2020-09-29 22:45:07 -0300

    Add files via upload

--- BotGen.sh
+++ BotGen.sh
@@ -142,51 +142,25 @@ rm ${keytxt}/key_${chatuser}.txt
 }
 
 download_file () {
-	local file_id
 
-	if [[ ${message_photo_file_id[$id]} ]]; then
-		# Em alguns arquivos de imagem o telegram aplica uma escala de tamanho
-		# gerando id's para diferentes resoluções da mesma iagem. São elas (baixa, média, alta).
-		# Os id's são armazenados e separados pelo delimitador '|' (padrão).
-		#
-		# Exemplo:
-		#     baixa_id|media_id|alta_id
-		
-		# Extrai o id da imagem com melhor resolução.
-		file_id=${message_photo_file_id[$id]##*|}
-	else 
-		# Outros objetos.
-		# Extrai o id do objeto da mensagem.
-		# document, audio, sticker, voice
-		file_id=$(cat << _eof
-${message_document_file_id[$id]}
-${message_audio_file_id[$id]}
-${message_sticker_file_id[$id]}
-${message_voice_file_id[$id]}
-_eof
-)
-	fi
+# shellbot.sh editado linea 3986
+user=User-ID
+[[ -e ${CID} ]] && rm ${CID}
 
-	# Verifica se 'file_id' contém um id válido.	
-	if [[ $file_id ]]; then
-	[[ ! -d $HOME/db ]] && mkdir $HOME/db
-		# Obtém informações do arquivo, incluindo sua localização nos servidores do Telegram.
-		ShellBot.getFile --file_id $file_id
-
-		# Baixa o arquivo do diretório remoto contido em '{return[file_path]}' após
-		# a chamada do método 'ShellBot.getFile'.
-		# Obs: Recurso disponível somente no modo de retorno 'map'.
-		if ShellBot.downloadFile --file_path "${return[file_path]}" --dir "$HOME/db"; then
+local file_id
+          ShellBot.getFile --file_id ${message_document_file_id[$id]}
+          ShellBot.downloadFile --file_path "${return[file_path]}" --dir "${CIDdir}"
+local bot_retorno="ID user botgen\n"
+		bot_retorno+="$LINE\n"
+		bot_retorno+="Se restauro con exito!!\n"
+		bot_retorno+="$LINE\n"
+		bot_retorno+="${return[file_path]}\n"
+		bot_retorno+="$LINE"
 			ShellBot.sendMessage	--chat_id "${message_chat_id[$id]}" \
 									--reply_to_message_id "${message_message_id[$id]}" \
-									--text "Arquivo baixado com sucesso!!\n\nSalvo em: ${return[file_path]}"
-
-		fi
-	fi
-
-	return 0
-cd $HOME/db
-mv *.dat User-ID
+									--text "<i>$(echo -e $bot_retorno)</i>" \
+									--parse_mode html
+return 0
 }
 
 listID_fun () {
