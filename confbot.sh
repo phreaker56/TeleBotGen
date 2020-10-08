874d16757cf38376ddeac12a9dc509dbd925928d
rudi9999 <67137156+rudi9999@users.noreply.github.com>
2020-10-07 22:02:03 -0300

    Add files via upload

--- confbot.sh
+++ confbot.sh
@@ -2,6 +2,7 @@
 
 SCPresq="aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL3J1ZGk5OTk5L1RlbGVCb3RHZW4vbWFzdGVyL3NvdXJjZXM="
 SUB_DOM='base64 -d'
+bar="\e[0;36m=====================================================\e[0m"
 
 check_ip () {
 MIP=$(ip addr | grep 'inet' | grep -v inet6 | grep -vE '127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | grep -o -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | head -1)
@@ -40,9 +41,9 @@ chmod +x ${ARQ}/$1
 
 download () {
 clear
-echo -e "$BARRA"
+echo -e "$bar"
 echo -e "\033[1;33mDescargando archivos... "
-echo -e "$BARRA"
+echo -e "$bar"
 cd $HOME
 REQUEST=$(echo $SCPresq|$SUB_DOM)
 wget -O "$HOME/lista-arq" ${REQUEST}/lista-bot > /dev/null 2>&1
@@ -60,28 +61,28 @@ done
 
 ini_token () {
 clear
-msg -bar
+echo -e "$bar"
 echo -e "  \033[1;37mIngrese el token de su bot"
-msg -bar
+echo -e "$bar"
 echo -n "TOKEN: "
 read opcion
 echo "$opcion" > ${CIDdir}/token
-msg -bar
-echo -e "  \033[1;32mtoken se guardo con exito!" && msg -bar && echo -e "  \033[1;37mPara tener acceso a todos los comandos del bot\n  deve iniciar el bot en la opcion 2.\n  desde su apps (telegram). ingresar al bot!\n  digite el comando \033[1;31m/id\n  \033[1;37mel bot le respodera con su ID de telegram.\n  copiar el ID e ingresar el mismo en la opcion 3" && msg -bar
+echo -e "$bar"
+echo -e "  \033[1;32mtoken se guardo con exito!" && echo -e "$bar" && echo -e "  \033[1;37mPara tener acceso a todos los comandos del bot\n  deve iniciar el bot en la opcion 2.\n  desde su apps (telegram). ingresar al bot!\n  digite el comando \033[1;31m/id\n  \033[1;37mel bot le respodera con su ID de telegram.\n  copiar el ID e ingresar el mismo en la opcion 3" && echo -e "$bar"
 read foo
 bot_gen
 }
 
 ini_id () {
 clear
-msg -bar
+echo -e "$bar"
 echo -e "  \033[1;37mIngrese su ID de telegram"
-msg -bar
+echo -e "$bar"
 echo -n "ID: "
 read opcion
 echo "$opcion" > ${CIDdir}/Admin-ID
-msg -bar
-echo -e "  \033[1;32mID guardo con exito!" && msg -bar && echo -e "  \033[1;37mdesde su apps (telegram). ingresar al bot!\n  digite el comando \033[1;31m/menu\n  \033[1;37mprueve si tiene acceso al menu extendido." && msg -bar
+echo -e "$bar"
+echo -e "  \033[1;32mID guardo con exito!" && echo -e "$bar" && echo -e "  \033[1;37mdesde su apps (telegram). ingresar al bot!\n  digite el comando \033[1;31m/menu\n  \033[1;37mprueve si tiene acceso al menu extendido." && echo -e "$bar"
 read foo
 bot_gen
 }
@@ -100,11 +101,11 @@ bot_gen
 
 ayuda_fun () {
 clear
-msg -bar
-echo -e "            \033[1;41m Instrucciones rapidas \033[0m"
-msg -bar
+echo -e "$bar"
+echo -e "            \e[47m\e[30m Instrucciones rapidas \e[0m"
+echo -e "$bar"
 echo -e "\033[1;37m   Es necesario crear un bot en \033[1;32m@BotFather "
-msg -bar
+echo -e "$bar"
 echo -e "\033[1;32m1- \033[1;37mEn su apps telegram ingrese a @BotFather"
 echo -e "\033[1;32m2- \033[1;37mDigite el comando \033[1;31m/newbot"
 echo -e "\033[1;32m3- @BotFather \033[1;37msolicitara que\n   asigne un nombre a su bot"
@@ -115,7 +116,7 @@ echo -e "\033[1;32m7- \033[1;37mPoner en linea el bot\n   en la opcion \033[1;32
 echo -e "\033[1;32m8- \033[1;37mEn su apps telegram, inicie el bot creado\n   digite el comando \033[1;31m/id \033[1;37mel bot le respondera\n   con su ID de telegran (copie el ID)"
 echo -e "\033[1;32m9- \033[1;37mIngrese el ID en la\n   opcion \033[1;32m[3] \033[1;31m> \033[1;37mID DE USUARIO TELEGRAM"
 echo -e "\033[1;32m10-\033[1;37mcomprueve que tiene acceso a\n   las opciones avanzadas de su bot."
-msg -bar
+echo -e "$bar"
 read foo
 bot_gen
 }
@@ -134,15 +135,16 @@ PID_GEN=$(ps x|grep -v grep|grep "BotGen.sh")
 [[ ! $PID_GEN ]] && PID_GEN="\033[1;31moffline" || PID_GEN="\033[1;32monline"
 
 CIDdir=/etc/ADM-db && [[ ! -d ${CIDdir} ]] && mkdir ${CIDdir}
-msg -bar
-echo -e "     \033[1;41m \033[1;37m>>>>>>  BotGen by \033[1;36mRufu99\033[1;32m  $(cat ${CIDdir}/vercion) \033[1;37m<<<<<< \033[0m"
-msg -bar
-echo -e "\033[1;32m[1] \033[1;31m> \033[1;37mTOKEN DEL BOT"
-echo -e "\033[1;32m[2] \033[1;31m> \033[1;37mINICIAR/PARAR BOT $PID_GEN\033[0m"
-echo -e "\033[1;32m[3] \033[1;31m> \033[1;37mID DE USUARIO TELEGRAM"
-echo -e "\033[1;32m[4] \033[1;31m> \033[1;37mMANUAL"
-msg -bar && echo -ne "$(msg -verd "[0]") $(msg -verm2 ">") "&& msg -bra "\033[1;41m\033[1;37m <<ATRAS "
-msg -bar
+echo -e "$bar"
+echo -e "     \e[47m \e[30m>>>>>>  BotGen by \e[1;36mRufu99\e[1;32m  $(cat ${CIDdir}/vercion)\e[0m\e[47m \e[30m<<<<<< \e[0m"
+echo -e "$bar"
+echo -e "\033[1;32m[1] \033[1;36m> \033[1;37mTOKEN DEL BOT"
+echo -e "\033[1;32m[2] \033[1;36m> \033[1;37mINICIAR/PARAR BOT $PID_GEN\033[0m"
+echo -e "\033[1;32m[3] \033[1;36m> \033[1;37mID DE USUARIO TELEGRAM"
+echo -e "\033[1;32m[4] \033[1;36m> \033[1;37mMANUAL"
+echo -e "$bar"
+echo -e "\e[1;32m[0] \e[36m>\e[0m \e[47m\e[30m <<ATRAS "
+echo -e "$bar"
 echo -n "Opcion: "
 read opcion
 case $opcion in
