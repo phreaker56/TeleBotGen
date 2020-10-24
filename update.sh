643fc16f18bb9e6e4963eff8c86ebbe5abf9c6dd
rudi9999 <67137156+rudi9999@users.noreply.github.com>
2020-10-24 00:33:02 -0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -86,17 +86,7 @@ read foo
 bot_gen
 }
 
-start_bot () {
-[[ ! -e "${CIDdir}/token" ]] && echo "null" > ${CIDdir}/token
-unset PIDGEN
-PIDGEN=$(ps aux|grep -v grep|grep "BotGen.sh")
-if [[ ! $PIDGEN ]]; then
-screen -dmS teleBotGen ${CIDdir}/BotGen.sh
-else
-killall BotGen.sh
-fi
-bot_gen
-}
+
 
 ayuda_fun () {
 clear
@@ -157,6 +147,11 @@ esac
 }
 
 update () {
+PID_GEN=$(ps x|grep -v grep|grep "BotGen.sh")
+[[ $PID_GEN ]] && start_bot
+
+[[ -d $HOME/sources ]] && rm -rf $HOME/sources
+
 cd $HOME
 REQUEST=$(echo $SCPresq|$SUB_DOM)
 wget -O "$HOME/lista-arq" ${REQUEST}/lista-bot > /dev/null 2>&1
@@ -171,13 +166,25 @@ fi
 
 veryfy_fun () {
 SRC="$HOME/botgen" && [[ ! -d ${SRC} ]] && mkdir ${SRC}
+SRC2="${SRC}/sources" && [[ ! -d ${SRC2} ]] && mkdir ${SRC2}
 unset ARQ
 case $1 in
 "BotGen.sh")ARQ="$HOME/botgen/";;
-*)ARQ="$HOME/botgen/";;
+*)ARQ="$HOME/${SRC2}/";;
 esac
 mv -f $HOME/$1 ${ARQ}/$1
 chmod +x ${ARQ}/$1
+}
+
+start_bot () {
+# [[ ! -e "${CIDdir}/token" ]] && echo "null" > ${CIDdir}/token
+unset PIDGEN
+PIDGEN=$(ps aux|grep -v grep|grep "BotGen.sh")
+if [[ ! $PIDGEN ]]; then
+screen -dmS teleBotGen ${CIDdir}/BotGen.sh
+else
+killall BotGen.sh
+fi
 }
 
 update
