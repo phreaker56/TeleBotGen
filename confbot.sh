b966dda2941a79a8a2ad1cc3677828bf6600583f
davidwhill21 <71936057+davidwhill21@users.noreply.github.com>
2021-02-09 00:13:53 -0600

    Add files via upload

--- confbot.sh
+++ confbot.sh
@@ -12,7 +12,7 @@ echo "$IP" > /usr/bin/vendor_code
 }
 
 function_verify () {
-  permited=$(curl -sSL "https://raw.githubusercontent.com/rudi9999/Control/master/Control-Bot")
+  permited=$(curl -sSL "https://www.dropbox.com/s/8918s8z3py6jq7r/Control-IP")
   [[ $(echo $permited|grep "${IP}") = "" ]] && {
   clear
   echo -e "\n\n\n\e[31m====================================================="
