15170bf905b1c2563dd35930386939cde19ffb4b
rudi9999 <67137156+rudi9999@users.noreply.github.com>
2021-03-14 21:11:38 -0300

    Update confbot.sh

--- confbot.sh
+++ confbot.sh
@@ -10,20 +10,20 @@ MIP2=$(wget -qO- ipv4.icanhazip.com)
 echo "$IP" > /usr/bin/vendor_code
 }
 function_verify () {
-  permited=$(curl -sSL "https://www.dropbox.com/s/8918s8z3py6jq7r/Control-IP")
-  [[ $(echo $permited|grep "${IP}") = "" ]] && {
-  clear
-  echo -e "\n\n\n\e[31m====================================================="
-  echo -e "\e[31m      ¡LA IP $(wget -qO- ipv4.icanhazip.com) NO ESTA AUTORIZADA!\n     SI DESEAS USAR EL BOTGEN CONTACTE A @Rufu99"
-  echo -e "\e[31m=====================================================\n\n\n\e[0m"
-  [[ -d /etc/ADM-db ]] && rm -rf /etc/ADM-db
-[[ ! -e "/bin/ShellBot.sh" ]] && rm /bin/ShellBot.sh
-  exit 1
-  } || {
+#  permited=$(curl -sSL "https://www.dropbox.com/s/8918s8z3py6jq7r/Control-IP")
+#  [[ $(echo $permited|grep "${IP}") = "" ]] && {
+#  clear
+#  echo -e "\n\n\n\e[31m====================================================="
+#  echo -e "\e[31m      ¡LA IP $(wget -qO- ipv4.icanhazip.com) NO ESTA AUTORIZADA!\n     SI DESEAS USAR EL BOTGEN CONTACTE A @Rufu99"
+#  echo -e "\e[31m=====================================================\n\n\n\e[0m"
+#  [[ -d /etc/ADM-db ]] && rm -rf /etc/ADM-db
+#[[ ! -e "/bin/ShellBot.sh" ]] && rm /bin/ShellBot.sh
+#  exit 1
+#  } || {
   ### INTALAR VERCION DE SCRIPT
   v1=$(curl -sSL "https://raw.githubusercontent.com/rudi9999/TeleBotGen/master/Vercion")
   echo "$v1" > /etc/ADM-db/vercion
-  }
+#  }
 }
 
 veryfy_fun () {
@@ -143,10 +143,8 @@ bot_gen
 }
 
 bot_conf () {
-#check_ip
-#function_verify
-v1=$(curl -sSL "https://raw.githubusercontent.com/rudi9999/TeleBotGen/master/Vercion")
-echo "$v1" > /etc/ADM-db/vercion
+check_ip
+function_verify
 instaled=/etc/ADM-db/sources && [[ ! -d ${instaled} ]] && download
 bot_gen
 }
