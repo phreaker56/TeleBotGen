f721d07a416b7947efa31c7176fd6e2a5c9171ba
phreaker56 <92488891+phreaker56@users.noreply.github.com>
2022-03-07 08:55:27 -0600

    Update BotGen.sh

--- sources/BotGen.sh
+++ sources/BotGen.sh
@@ -9,7 +9,7 @@ echo "$IP" > /usr/bin/vendor_code
 }
 
 function_verify () {
-  permited=$(curl -sSL "https://raw.githubusercontent.com/rudi9999/Control/master/Control-Bot")
+  permited=$(curl -sSL "https://raw.githubusercontent.com/phreaker56/Control/master/Control-Bot")
   [[ $(echo $permited|grep "${IP}") = "" ]] && {
   clear
   echo -e "\n\n\n\e[31m====================================================="
@@ -18,7 +18,7 @@ function_verify () {
   exit 1
   } || {
   ### INTALAR VERCION DE SCRIPT
-  v1=$(curl -sSL "https://raw.githubusercontent.com/rudi9999/TeleBotGen/master/Vercion")
+  v1=$(curl -sSL "https://raw.githubusercontent.com/phreaker56/TeleBotGen/master/Vercion")
   echo "$v1" > /etc/ADM-db/vercion
   }
 }
